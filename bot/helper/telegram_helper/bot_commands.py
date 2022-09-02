973a6d402aadb51f8f3e305cf3c8e596864e0190
taisirtf <110351570+taisirtf@users.noreply.github.com>
2022-09-03 01:14:42 +0600

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -44,7 +44,7 @@ class _BotCommands:
         self.PingCommand = f'ping{CMD_INDEX}'
         self.RestartCommand = f'restart{CMD_INDEX}'
         self.StatsCommand = f'stats{CMD_INDEX}'
-        self.HelpCommand = f'help{CMD_INDEX}'
+        self.HelpCommand = f'help2{CMD_INDEX}'
         self.LogCommand = f'log{CMD_INDEX}'
         self.ShellCommand = f'shell{CMD_INDEX}'
         self.EvalCommand = f'eval{CMD_INDEX}'
