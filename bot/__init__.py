6b96bc3c0f9f30d6e2e81bd3b9ed03d0516d91a7
taisirtf <110351570+taisirtf@users.noreply.github.com>
2022-09-05 11:33:38 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -561,8 +561,8 @@ try:
     if len(START_BTN1_NAME) == 0 or len(START_BTN1_URL) == 0:
         raise KeyError
 except:
-    START_BTN1_NAME = 'Repo'
-    START_BTN1_URL = 'https://github.com/arshsisodiya/helios-mirror'
+    START_BTN1_NAME = 'Log Channel✅'
+    START_BTN1_URL = 'https://t.me/moviehubbdleech'
 
 try:
     START_BTN2_NAME = getConfig('START_BTN2_NAME')
@@ -570,8 +570,8 @@ try:
     if len(START_BTN2_NAME) == 0 or len(START_BTN2_URL) == 0:
         raise KeyError
 except:
-    START_BTN2_NAME = 'Support Group'
-    START_BTN2_URL = 'https://t.me/mirrorsociety'
+    START_BTN2_NAME = 'Support Group✅'
+    START_BTN2_URL = 'https://t.me/moviehubbd'
 updater = tgUpdater(token=BOT_TOKEN, request_kwargs={'read_timeout': 20, 'connect_timeout': 15})
 bot = updater.bot
 dispatcher = updater.dispatcher
