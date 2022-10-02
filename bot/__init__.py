e7c5592011aaa7e3d41890b5403b4292a3501ee3
Moviehubbd <112174137+Moviehubbd@users.noreply.github.com>
2022-10-03 00:41:08 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -60,7 +60,7 @@ except:
 
 PORT = environ.get('PORT')
 Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT}", shell=True)
-srun(["firefox", "-d", "--profile=."])
+srun(["chrome", "-d", "--profile=."])
 if not ospath.exists('.netrc'):
     srun(["touch", ".netrc"])
 srun(["cp", ".netrc", "/root/.netrc"])
